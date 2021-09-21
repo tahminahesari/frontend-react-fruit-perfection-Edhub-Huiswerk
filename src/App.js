7fe81d22dfd8045397d74f592a49fff31bfadeea
tahminahesari <hesari@live.com>
2021-09-21 21:30:00 +0200

    opdracht1af

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <main>
-        <article>
-            <article>
-                <img
-                    src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
-                    alt="Citroenen”
-          />
+    return (
+        <main>
+            <article className="product ">
+                <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"alt="Citroenen"/>
           <h2>Citroen</h2>
-            <h2>Cintroen</h2>
-            <p>
-            "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-            </p>
+          <p>
+             Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
+          </p>
         </article>
-    </main>
-
+      </main>
   );
 }
+
+export default App;
