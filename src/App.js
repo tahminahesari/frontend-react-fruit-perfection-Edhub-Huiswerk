115cec940b4dca1be6b99d885b3cb937f998beee
tahminahesari <hesari@live.com>
2021-09-21 20:40:51 +0200

    opdracht1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Begin hier met de tutorial!
-    </div>
+    <main>
+        <article>
+            <article>
+                <img
+                    src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+                    alt="Citroenen”
+          />
+          <h2>Citroen</h2>
+            <h2>Cintroen</h2>
+            <p>
+            "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+            </p>
+        </article>
+    </main>
+
   );
 }
-
-export default App;
